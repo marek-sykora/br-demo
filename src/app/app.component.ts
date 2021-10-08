6477ef2a56726d00b0c9055ecafd4d38cba75c1d
Marek Sýkora <marek.sykora2@zf.com>
2021-10-08 08:30:47 +0200

    PrimeNG ui kit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  cities: any[];
+  selectedCity: any;
+
+  constructor() {
+      this.cities = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
+      ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import {DropdownModule} from 'primeng/dropdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    DropdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
